21729367ca61c9d0d73d13e00491fe24f37cd4d7
Si Taggart <me@simontaggart.com>
2019-10-19 17:13:30 -0700

    chore: update ts config

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -10,6 +10,15 @@ module.exports = {
     'prettier/@typescript-eslint',
   ],
   rules: {
+    'arrow-parens': [
+      'error',
+      'as-needed',
+      {
+        requireForBlockBody: false,
+      },
+    ],
+    'arrow-body-style': ['error', 'as-needed'],
+    'comma-dangle': ['error', 'only-multiline'],
     'prettier/prettier': ['error', { parser: 'typescript' }],
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
